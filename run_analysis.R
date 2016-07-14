13a60618fb5c2ea4f321954c0a54f54692351987
=IElgohary <eslam.elgohary@hotmail.ca>
2016-07-14 06:19:22 +0200

    added finaldata.txt

--- run_analysis.R
+++ run_analysis.R
@@ -72,11 +72,6 @@ colnames(data) <- columns
 ######################################################################
 # 5.Create a second, independent tidy data set with the average of each variable for each activity and each subject
 
-# melt dataset
-#dataMelt <- melt(data, id = c("activityId", "subjectId"),na.rm=TRUE)
-
-# cast dataset with averaging
-#dataWithAverage <- dcast(dataMelt, activityId + subjectId ~ variable, mean)
 dataWithAverage<- ddply(data, .(subjectId, activityId), function(x) colMeans(x[, 1:66]))
 #create new file with the new dataset
 write.table(dataWithAverage, "finalData.txt", row.names = FALSE)
